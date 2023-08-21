c1702f73468c1e50b84ff2ccdc5d88a1c1abc06c
Alex Krestyannikov <avocodefreetrial@yandex.ru>
2023-08-21 20:18:41 +0500

    выполнил задание 3(простое), добавил prompt'ы в переменные, вывел скидки по условию

--- script.js
+++ script.js
@@ -15,3 +15,32 @@ console.log("Стоимость верстки экранов " + screenPrice + 
 console.log("Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани");
 console.log(screens.toLocaleLowerCase().split(','));
 console.log("Процент отката посреднику за работу: " + (fullPrice * (rollback / 100)));
+
+title = prompt("Как называется ваш проект?");
+screens = prompt(
+  "Какие типы экранов нужно разработать?",
+  "Простые, Сложные, Интерактивные"
+);
+screenPrice = prompt("Сколько будет стоить данная работа?", "12000");
+adaptive = confirm("Нужен ли адаптив на сайте?");
+
+const service1 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice1 = prompt("Сколько это будет стоить?");
+const service2 = prompt("Какой дополнительный тип услуги нужен?");
+const servicePrice2 = prompt("Сколько это будет стоить?");
+
+fullPrice = parseInt(screenPrice) + parseInt(servicePrice1) + parseInt(servicePrice2);
+
+const servicePercentPrice = fullPrice - Math.ceil(fullPrice * (rollback / 100));
+
+console.log(servicePercentPrice);
+
+if(fullPrice > 30000) {
+  console.log("Даем скидку в 10%");
+} else if(fullPrice >= 15000 && fullPrice <= 30000) {
+  console.log("Даем скидку в 5%");
+} else if(fullPrice < 15000 && fullPrice >= 0) {
+  console.log("Скидка не предусмотрена");
+} else {
+  console.log("Что то пошло не так");
+}
