8faafd71bf3e7251b0b60102c259bbc88abf93fc
Alex Krestyannikov <avocodefreetrial@yandex.ru>
2023-08-22 20:12:32 +0500

    рефакторинг

--- conditions.js
+++ conditions.js
@@ -1,23 +1,6 @@
 let lang = prompt("Введите язык", "ru или en");
 
-if(lang == "ru") {
-  console.log("Понедельник\nВторник\nСреда\nЧетверг\nПятница\nСуббота\nВоскресенье");
-} else if(lang == "en") {
-    console.log("Monday\nTuesday\nWednesday\nThursday\nFriday\nSaturday\nSunday");
-  }
-
-switch (lang) {
-  case "ru":
-      console.log(
-        "Понедельник\nВторник\nСреда\nЧетверг\nПятница\nСуббота\nВоскресенье"
-      );
-    break;
-  case "en":
-    console.log(
-      "Monday\nTuesday\nWednesday\nThursday\nFriday\nSaturday\nSunday"
-      );
-}
-
+const namePerson = prompt("Введите ваше имя");
 const daysOfWeek = [
   [
     "Понедельник",
@@ -39,8 +22,24 @@ const daysOfWeek = [
   ],
 ];
 
-lang == "ru" ? console.log(daysOfWeek[0]) : lang == "en" ? console.log(daysOfWeek[1]) : null;
+if(lang == "ru") {
+  console.log("Понедельник\nВторник\nСреда\nЧетверг\nПятница\nСуббота\nВоскресенье");
+} else if(lang == "en") {
+    console.log("Monday\nTuesday\nWednesday\nThursday\nFriday\nSaturday\nSunday");
+  }
 
-const namePerson = prompt("Введите ваше имя");
+switch (lang) {
+  case "ru":
+      console.log(
+        "Понедельник\nВторник\nСреда\nЧетверг\nПятница\nСуббота\nВоскресенье"
+      );
+    break;
+  case "en":
+    console.log(
+      "Monday\nTuesday\nWednesday\nThursday\nFriday\nSaturday\nSunday"
+      );
+}
+
+lang == "ru" ? console.log(daysOfWeek[0]) : lang == "en" ? console.log(daysOfWeek[1]) : null;
 
 namePerson == "Артем" ? console.log("директор") : namePerson == "Александр" ? console.log("преподаватель") : console.log("студент");
