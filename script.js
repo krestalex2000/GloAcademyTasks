d377c36ed0f069d482ea1b1825da72af423eaf7a
Alex Krestyannikov <avocodefreetrial@yandex.ru>
2023-09-10 16:07:54 +0500

    выполниз задание 15(простое)

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const DomElement = function(selector, height, width, bg, fontSize, text) {
+  this.selector = selector,
+  this.height = height,
+  this.width = width,
+  this.bg = bg,
+  this.fontSize = fontSize,
+  this.text = text,
+
+  this.elementCreated = function() {
+
+    const elementStyles = `height: ${this.height}px; width: ${this.width}px; background: ${this.bg}; font-size: ${this.fontSize}px;`
+   
+    if(this.selector[0] === '.') {
+      const div = document.createElement('div');
+      div.classList.add((this.selector.slice(1)));
+      div.textContent = this.text;
+      div.style.cssText = elementStyles;
+
+      document.body.prepend(div)
+    }
+
+    if(this.selector[0] === '#') {
+      const p = document.createElement('p');
+      p.id = this.selector.slice(1);
+      p.textContent = this.text;
+      p.style.cssText = elementStyles;
+
+      document.body.prepend(p)
+    }
+  }
+}
+
+const newElement = new DomElement('#paragraph', 50, 400, 'cian', 36, 'Hello World')
+
