34bc63068f10164348db580a8fe71a13d7cb98de
Alex Krestyannikov <avocodefreetrial@yandex.ru>
2023-09-10 17:29:28 +0500

    выполнил задание 15(сложное)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const DomElement = function(selector, height, width, bg, fontSize, text) {
+const DomElement = function(selector, height, width, bg, fontSize, text, style) {
   this.selector = selector,
   this.height = height,
   this.width = width,
@@ -10,7 +10,7 @@ const DomElement = function(selector, height, width, bg, fontSize, text) {
 
   this.elementCreated = function() {
 
-    const elementStyles = `height: ${this.height}px; width: ${this.width}px; background: ${this.bg}; font-size: ${this.fontSize}px;`
+    const elementStyles = `height: ${this.height}px; width: ${this.width}px; background: ${this.bg}; font-size: ${this.fontSize}px; ${style}`
    
     if(this.selector[0] === '.') {
       const div = document.createElement('div');
@@ -18,7 +18,7 @@ const DomElement = function(selector, height, width, bg, fontSize, text) {
       div.textContent = this.text;
       div.style.cssText = elementStyles;
 
-      document.body.prepend(div)
+      return div
     }
 
     if(this.selector[0] === '#') {
@@ -27,10 +27,37 @@ const DomElement = function(selector, height, width, bg, fontSize, text) {
       p.textContent = this.text;
       p.style.cssText = elementStyles;
 
-      document.body.prepend(p)
+      return p
     }
   }
 }
 
-const newElement = new DomElement('#paragraph', 50, 400, 'cian', 36, 'Hello World')
+const moveElement = (event) => {
+
+  const elem = squareElement.getBoundingClientRect();
+  let x = elem.x;
+  let y = elem.y;
+
+
+  if (event.code != "ArrowRight" && event.code != "ArrowLeft" &&
+        event.code != "ArrowUp" && event.code != "ArrowDown") return;
+
+  if (event.code == "ArrowRight") x += 10;
+  if (event.code == "ArrowLeft") x -= 10; 
+  if (event.code == "ArrowUp") y -= 10;   
+  if (event.code == "ArrowDown") y += 10;
+
+  squareElement.style.left = x + 'px';
+  squareElement.style.top = y + 'px';
+}
+
+
+const newElement = new DomElement('#paragraph', 50, 400, 'yellow', 36, 'Hello World');
+const square = new DomElement('.square', 100, 100, 'red', '', '', 'position: absolute');
+const squareElement = square.elementCreated();
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.prepend(squareElement);
+  document.addEventListener('keydown', moveElement)
+})
 
